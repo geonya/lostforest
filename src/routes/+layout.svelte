a2afe3c224cb55817b7ebfcb0fd8a41b376fbbb9
geonya <geony8410@gmail.com>
2023-06-06 14:13:54 +0900

    scroll snap effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,58 @@
 <script lang="ts">
 	import '../global.css';
+
+	const scrollTo = (id: string) => {
+		const el = document.getElementById(id);
+		if (el) {
+			el.scrollIntoView({ behavior: 'smooth' });
+		}
+	};
 </script>
 
 <header>
 	<nav>
-		<a href="/">Logo</a>
-		<a href="/about">Menu</a>
-		<a href="/about">Reservation</a>
-		<a href="/about">Contact</a>
+		<a id="Logo" href="/" on:click|preventDefault={() => scrollTo('Main')}>Logo</a>
+		<div class="nav-content">
+			<a href="/" on:click|preventDefault={() => scrollTo('About')}>About</a>
+			<a href="/" on:click|preventDefault={() => scrollTo('Menu')}>Menu</a>
+			<a href="/" on:click|preventDefault={() => scrollTo('Reservation')}>Resrevation</a>
+		</div>
+		<a href="/" on:click|preventDefault={() => scrollTo('Contact')}>Contact</a>
 	</nav>
 </header>
 <slot />
 
 <style>
 	header {
-		position: absolute;
+		position: fixed;
 		top: 0;
 		left: 0;
 		width: 100%;
 		height: 3rem;
-		z-index: 2;
+		z-index: 10;
 	}
 	nav {
 		display: flex;
+		height: 100%;
 		justify-content: space-between;
 		align-items: center;
 		padding: 0 1rem;
+	}
+	#Logo {
+		font-size: 1.1rem;
+		font-weight: bold;
+	}
+	nav a {
+		font-size: 0.9rem;
+	}
+	.nav-content {
+		display: flex;
+	}
+
+	.nav-content a {
+		margin-right: 1rem;
+	}
+	.nav-content a:last-child {
+		margin-right: 0;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,86 @@
 <script lang="ts">
 </script>
 
-<hero>
-	<section class="hero_content">
-		<div />
-		<div>
-			<h1>Lost Forest</h1>
-			<h4>Only the lost find a new way</h4>
-		</div>
-		<h3>2023.07 Coming Soon</h3>
-	</section>
-</hero>
-
-<video src="/videos/bg-forest.mp4" autoplay muted loop playsinline />
+<main>
+	<article id="Main">
+		<video src="/videos/bg-forest.mp4" autoplay muted loop playsinline />
+		<section class="section_content">
+			<div />
+			<div>
+				<h1>Lost Forest</h1>
+				<h4>Only the lost find a new way</h4>
+			</div>
+			<h3>2023.07 Coming Soon</h3>
+		</section>
+	</article>
+	<article id="About">
+		<section class="section_content">
+			<div />
+			<div>
+				<h2>Only the lost find a new way</h2>
+				<h4>잃어버린 사람만이 새로운 길을 찾는다.</h4>
+			</div>
+			<h3>나만의 숲속 작은 산장, Lost Forest</h3>
+		</section>
+	</article>
+	<article id="Reservation" style="background-color:#444;">
+		<section class="section_content">
+			<div />
+			<div>
+				<h2>Reservation</h2>
+				<h4>우리만의 공간 Lost Foreset</h4>
+			</div>
+			<h3>나만의 숲속 작은 산장, Lost Forest</h3>
+		</section>
+	</article>
+	<article id="Contact">
+		<section class="section_content">
+			<div />
+			<div>
+				<h2>Contact</h2>
+				<h4>010-2382-8410</h4>
+				<h4>lostforestcafe@naver.com</h4>
+			</div>
+			<h3>나만의 숲속 작은 산장, Lost Forest</h3>
+		</section>
+	</article>
+</main>
 
 <style>
-	hero {
+	main {
+		overflow-y: scroll;
+		scroll-snap-type: y mandatory;
+		-webkit-overflow-scrolling: touch;
+		height: 100vh;
+	}
+	article {
+		scroll-snap-align: start;
+		/* only supported in Chrome */
+		scroll-snap-stop: always;
 		width: 100%;
 		height: 100vh;
+		position: relative;
 		display: grid;
 		place-items: center;
+		background-color: #333;
 	}
-	.hero_content {
+	section {
 		height: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
 		align-items: center;
 		text-align: center;
+		z-index: 2;
 	}
-
 	video {
-		position: fixed;
+		position: absolute;
 		top: 0;
 		left: 0;
 		width: 100%;
 		height: auto;
 		object-fit: cover;
-		z-index: -1;
+		z-index: 1;
 	}
 	@media (min-aspect-ratio: 16/9) {
 		video {
@@ -52,61 +95,77 @@
 		}
 	}
 	@media (max-width: 375px) {
-		.hero_content h1 {
+		section h1 {
 			font-size: 1rem;
 			letter-spacing: 0.5rem;
 		}
-		.hero_content h4 {
+		section h2 {
+			font-size: 1.5rem;
+			letter-spacing: 0.6rem;
+		}
+		section h4 {
 			font-size: 0.5rem;
 			letter-spacing: 0.2rem;
 		}
-		.hero_content h3 {
+		section h3 {
 			font-size: 0.7rem;
 			letter-spacing: 0.3rem;
 			margin-bottom: 4rem;
 		}
 	}
 	@media (min-width: 375px) {
-		.hero_content h1 {
+		section h1 {
 			font-size: 2rem;
 			letter-spacing: 0.5rem;
 		}
-		.hero_content h4 {
+		section h2 {
+			font-size: 1.5rem;
+			letter-spacing: 0.4rem;
+		}
+		section h4 {
 			font-size: 0.8rem;
 			letter-spacing: 0.2rem;
 		}
-		.hero_content h3 {
+		section h3 {
 			font-size: 0.8rem;
 			letter-spacing: 0.3rem;
 			margin-bottom: 4rem;
 		}
 	}
 	@media (min-width: 768px) {
-		.hero_content h1 {
+		section h1 {
 			font-size: 3rem;
 			letter-spacing: 0.5rem;
 		}
-		.hero_content h4 {
-			font-size: 1.2rem;
+		section h2 {
+			font-size: 2rem;
+			letter-spacing: 0.4rem;
+		}
+		section h4 {
+			font-size: 1rem;
 			letter-spacing: 0.2rem;
 		}
-		.hero_content h3 {
-			font-size: 1.5rem;
+		section h3 {
+			font-size: 1.2rem;
 			letter-spacing: 0.7rem;
 			margin-bottom: 4rem;
 		}
 	}
 	@media (min-width: 1200px) {
-		.hero_content h1 {
+		section h1 {
 			font-size: 4rem;
 			letter-spacing: 1.5rem;
 		}
-		.hero_content h4 {
-			font-size: 1.5rem;
+		section h2 {
+			font-size: 3rem;
+			letter-spacing: 1rem;
+		}
+		section h4 {
+			font-size: 1rem;
 			letter-spacing: 0.6rem;
 		}
-		.hero_content h3 {
-			font-size: 2rem;
+		section h3 {
+			font-size: 1.2rem;
 			letter-spacing: 0.3rem;
 			margin-bottom: 4rem;
 		}
